290a5692cd58d18621b7135afb803c3a617f14bf
Eric Landheer <e.landheer@mentechmedia.nl>
2021-07-30 08:41:22 +0200

    :rainbow: Require login / auth token for CreateUser mutation

--- tests/Feature/GraphQL/Users/CreateUserGraphQLTest.php
+++ tests/Feature/GraphQL/Users/CreateUserGraphQLTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature\GraphQL\Users;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -16,11 +17,18 @@ class CreateUserGraphQLTest extends TestCase
     use WithFaker;
     use MakesGraphQLRequests;
 
+    const TOKEN_NAME = 'test-token';
+
+    public $user;
+    public $token;
     public $data;
 
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->user = User::factory()->create();
+        $this->token = $this->user->createToken(self::TOKEN_NAME);
 
         $this->data = [
             'name' => $this->faker->name,
@@ -32,18 +40,28 @@ class CreateUserGraphQLTest extends TestCase
     /** @test */
     public function it_creates_user()
     {
-        $this->assertDatabaseCount('users', 0);
+        $this->assertDatabaseCount('users', 1);
 
         $response = $this->createUser($this->data);
 
         $response->assertSuccessful();
 
-        $this->assertDatabaseCount('users', 1);
+        $this->assertDatabaseCount('users', 2);
 
         $this->assertDatabaseHas('users', [
             'name' => $this->data['name'],
             'email' => $this->data['email'],
         ]);
+    }
+
+    /** @test */
+    public function it_requires_login()
+    {
+        $response = $this->createUser($this->data, false);
+
+        $data = json_decode($response->getContent());
+
+        $this->assertEquals('Unauthenticated.', $data->errors[0]->message);
     }
 
     /** @test */
@@ -66,9 +84,15 @@ class CreateUserGraphQLTest extends TestCase
         );
     }
 
-    public function createUser(array $data): TestResponse
+    public function createUser(array $data, bool $addAuthorization = true): TestResponse
     {
-        return $this->graphQL(/** @lang GraphQL */'
+        $authorization = [];
+
+        if($addAuthorization) {
+            $authorization['Authorization'] = sprintf('Bearer %s', $this->token->plainTextToken);
+        }
+
+        return $this->withHeaders($authorization)->graphQL(/** @lang GraphQL */'
             mutation CreateUser($name: String!, $email: String!, $password: String!) {
               createUser(input: {
                 name: $name,
